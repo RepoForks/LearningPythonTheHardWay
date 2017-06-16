47f13176e83622b34059957da3c703f6c8f1775a
Shivam Satija <dr.droid27@gmail.com>
2017-06-16 17:30:41 +0530

    Exercise one completed

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+
+# LearningPythonTheHardWay
+# Exercise One
+
+print "Hello world!"
+print "I'm Shivam Satija"
+print "Who am I? Android Developer"
+print "What I do? Build awesome android apps"
+print "Am I passionate about coding? Yeah!"
+
